7c2301268960ba41208b945aede6b276a279df27
ByeongChang <like98100@naver.com>
2025-08-22 17:14:59 +0900

    [chore] 로그 그룹 존재 여부 케이스에 따른 코드 분류

--- terraform/modules/codepipeline/main.tf
+++ terraform/modules/codepipeline/main.tf
@@ -95,8 +95,16 @@ resource "aws_cloudwatch_log_group" "codebuild" {
   })
 }
 
-data "aws_cloudwatch_log_group" "ecs_task" {
-  name = "/ecs/${var.project_name}-${var.environment}"
+# 로그 그룹이 있을 때
+# data "aws_cloudwatch_log_group" "ecs_task" {
+#   name = "/ecs/${var.project_name}-${var.environment}"
+# }
+
+# 로그 그룹이 없을 때
+resource "aws_cloudwatch_log_group" "ecs_task" {
+  name              = "/ecs/${var.project_name}-${var.environment}"
+  retention_in_days = 7
+  tags              = var.tags
 }
 
 
