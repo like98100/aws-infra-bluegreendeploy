bbf56f7aa216bf4beeab1aaf82e3b2221314efac
ByeongChang <like98100@naver.com>
2025-08-23 22:21:07 +0900

    [chore] alb 인바운드 그룹 변경

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -12,6 +12,14 @@ resource "aws_security_group" "alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+    ingress {
+    description = "HTTP"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     description = "HTTPS"
     from_port   = 443
